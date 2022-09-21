81164389ea05c900c7a06b4f3ae3d19d8aa2f990
liveacid <danny@liveacid.com>
2022-09-21 16:53:40 -0600

    added multi entry points

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const SupportTicketController =  require('./controller');
+const SupportTicketApi =  require('./routes');
+
+module.exports = {
+    SupportTicketController,
+    SupportTicketApi,
+};
--- index.js
+++
@@ -1,9 +0,0 @@
-const SupportTicketController =  require('./api/controller');
-const SupportTicketApi =  require('./api/routes');
-const { SupportTicket } = require('./dist');
-
-module.exports = {
-    SupportTicketController,
-    SupportTicketApi,
-    SupportTicket
-};
